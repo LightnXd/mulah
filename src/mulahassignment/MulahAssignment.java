53e56be91c0cb0502e994e7a482b63a5de53b672
LightnXd <149866487+LightnXd@users.noreply.github.com>
2024-07-17 14:45:39 +0800

    fix all bug found

---
+++ src/mulahassignment/MulahAssignment.java
@@ -0,0 +1,18 @@
+package mulahassignment;
+
+
+/**
+ *
+ * @author user
+ */
+public class MulahAssignment {
+
+    public static void main(String[] args) {
+        
+          SelectionPage page = new SelectionPage();
+          page.setVisible(true);
+        
+        
+    }
+    
+}
